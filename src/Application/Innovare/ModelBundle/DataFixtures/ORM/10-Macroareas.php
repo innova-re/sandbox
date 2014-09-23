d683c1d1bf54908a9a0bd39ef84e27d4320a55ae
Antonio Pierro <antonio.pierro@gmail.com>
2014-09-23 22:21:21 +0200

    added the Macroareas fixtures

---
+++ src/Application/Innovare/ModelBundle/DataFixtures/ORM/10-Macroareas.php
@@ -0,0 +1,35 @@
+<?php
+// src/Innovare/ModelBundle/DataFixtures/ORM/10-Macroareas.php
+
+namespace Innovare\ModelBundle\DataFixtures\ORM;
+
+use Innovare\ModelBundle\Entity\Macroarea;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixturesInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/*
+ * Fixture for the Macroarea Entity
+ */
+class LoadUserData implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 10;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $macroarea1 = new Macroarea();
+        $macroarea1->setNome('Agroalimentare');
+
+        $manager->persist($macroarea1);
+        $manager->flush();
+    }
+}
