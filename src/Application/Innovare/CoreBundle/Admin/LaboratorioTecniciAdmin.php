5888199221afc3a011fd425bdbd8ea97cbbfab38
Antonio Pierro <antonio.pierro@gmail.com>
2014-10-03 22:34:45 +0200

    Generated the sonata admin for the LaboratorioTecnici Entity

---
+++ src/Application/Innovare/CoreBundle/Admin/LaboratorioTecniciAdmin.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Application\Innovare\CoreBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class LaboratorioTecniciAdmin extends Admin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('nome')
+            ->add('cognome')
+            ->add('matricola')
+            ->add('noteTecnico')
+            ->add('id')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('nome')
+            ->add('cognome')
+            ->add('matricola')
+            ->add('noteTecnico')
+            ->add('id')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('nome')
+            ->add('cognome')
+            ->add('matricola')
+            ->add('noteTecnico')
+            ->add('id')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('nome')
+            ->add('cognome')
+            ->add('matricola')
+            ->add('noteTecnico')
+            ->add('id')
+        ;
+    }
+}
