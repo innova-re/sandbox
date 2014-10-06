624ce0d08fc18440e16fe85dc7af91539876ef74
Antonio Pierro <antonio.pierro@gmail.com>
2014-10-06 10:37:45 +0200

    Generated the admin for the Ente Entity

---
+++ src/Application/Innovare/CoreBundle/Admin/EnteAdmin.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Application\Innovare\CoreBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class EnteAdmin extends Admin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('nome')
+            ->add('descrizione')
+            ->add('id')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('nome')
+            ->add('descrizione')
+            ->add('id')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('nome')
+            ->add('descrizione')
+            ->add('id')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('nome')
+            ->add('descrizione')
+            ->add('id')
+        ;
+    }
+}
