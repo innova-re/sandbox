9e1d7559d011bc749830e7e0984972cbe5d5cbc0
Antonio Pierro <antonio.pierro@gmail.com>
2014-10-06 10:48:02 +0200

    Added the Support Preview Mode to the EnteAdmin class

--- src/Application/Innovare/CoreBundle/Admin/EnteAdmin.php
+++ src/Application/Innovare/CoreBundle/Admin/EnteAdmin.php
@@ -10,6 +10,8 @@ use Sonata\AdminBundle\Show\ShowMapper;
 
 class EnteAdmin extends Admin
 {
+    public $supportsPreviewMode = true;
+
     /**
      * @param DatagridMapper $datagridMapper
      */
